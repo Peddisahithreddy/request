4d68a5c2698b9fc0f713694398089a686ed59a18
Sahith Peddi <sahithreddy@astreya.com>
2023-12-12 10:56:09 +0530

    notifications

--- face-recognizer-frontend/src/app/notification/notification.component.ts
+++ face-recognizer-frontend/src/app/notification/notification.component.ts
@@ -13,13 +13,24 @@ import { MarkleaveserviceService } from '../markleaveservice.service';
 export class NotificationComponent implements OnInit{
   girl_icon="../assets/girl-icon.png"
   boy_icon="../assets/user-icon.png"
-  users!: string;
+  users: string[] =[];
+
+  username!: string;
   result!: string;
   //id: number = history.state.data[0];
   //email: string = history.state.data[2];
   constructor(private router: Router,private markleaveService: MarkleaveserviceService,private userService: UserserviceService) {}
   ngOnInit(): void {
-    this.markleaveService.getRequest().subscribe((response) => {this.users = response,console.log(response)},
+    this.markleaveService.getRequest().subscribe((response) =>
+    {
+
+      for(let i = 0; i<response.requests.length; i++){
+        console.log(response.requests[i].emp_name)
+        this.users.push(response.requests[i].emp_name);
+        }
+      // this.username = this.users,
+    //  console.log(response.requests[0].emp_name)
+    },
 
 
     )
